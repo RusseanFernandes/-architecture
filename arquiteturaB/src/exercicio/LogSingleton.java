db35bf3b1b08a87ffa7d83367346c6aa65e07dfb
Russean <Russean@10.10.146.208>
2019-11-23 12:58:10 -0300

    Commit

--- arquiteturaB/src/exercicio/LogSingleton.java
+++ arquiteturaB/src/exercicio/LogSingleton.java
@@ -6,44 +6,36 @@ import java.util.Collection;
 public class LogSingleton {
 	private static LogSingleton instance;
 	private Collection<String> textos;
+	private Object getTextos;
 
 	private LogSingleton() {
 	
 	}
 
 	public static LogSingleton getInstance() {
+		
 		if(instance ==null) {
 			instance = new LogSingleton();
 		}
-		
+		return instance;
 	}
-	public Collection<String> getTexto() {
+	public Collection<String> getTextos() {
 		return textos;
 	}
 
-	public void setTexto(Collection<String> texto) {
-		this.textos = texto;
+	public void setTextos(Collection<String> textos) {
+		this.textos = textos;
 	}
 	
 	public static void main(String[] args) {
-		Log log = new Log();
+		LogSingleton log = LogSingleton.getInstance();
 		log.setTextos(new ArrayList<String>());
 		
 		log.getTextos.add("Log1");	
 		log.getTextos.add("Log2");
-		log.gettextos.add("Log3");
+		log.getTextos.add("Log3");
 		
-		Log log2 = new Log();
-		log.setTextos(new ArrayList<String>());
-		
-		LogSingleton.getInstance();
-		
-		log2.getTextos.add("Log4");	
-		log2.getTextos.add("Log5");
-		log2.gettextos.add("Log61");
-		}	
-	
-	
-	}
+
+		}		
 	
 }
---
+++ arquiteturaB/src/projeto/professor/RepositorioQuarto.java
@@ -0,0 +1,10 @@
+package projeto.professor;
+
+public class RepositorioQuarto {
+
+	public void criarQuarto(Quarto quarto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

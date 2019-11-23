6323835768acdf6320dfcf4015ee83410e3b2ef0
Russean <Russean@10.10.146.208>
2019-11-23 09:00:30 -0300

    commit

---
+++ arquiteturaB/src/projeto/professor/FabricaClasseApart.java
@@ -0,0 +1,17 @@
+package projeto.professor;
+
+public class FabricaClasseApart implements IFabricaClasse{
+
+	@Override
+	public Hotel criarHotel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Quarto criarQuarto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
